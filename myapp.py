dfad4961bbc40a5e9f81bb84be9ff366667e521e
nenaslaats <47478636+nenaslaats@users.noreply.github.com>
2020-05-24 19:48:11 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -120,7 +120,7 @@ def create_figure():
     r3_sol=four_body_sol[:,6:9]
     r4_sol=four_body_sol[:,9:12]
     
-    fig = plt.figure()
+    fig = plt.figure(figsize=(20,30), dpi = 300)
     ax = p3.Axes3D(fig)
     #Plot van r
     line1, = ax.plot(r1_sol[:,0],r1_sol[:,1],r1_sol[:,2],color="darkblue")
