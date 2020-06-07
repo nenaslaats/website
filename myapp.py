439f92ad9e8d0e7b51ad17410364b709ac859699
nenaslaats <47478636+nenaslaats@users.noreply.github.com>
2020-06-07 17:51:51 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -24,6 +24,14 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template('index.html')
+def my_form():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
 
 @app.route('/plot.png')
 def plot_png():
